f3e24ab1462c75a12990f76c573813f863722e7a
benpeng <961888137@qq.com>
2022-07-14 08:57:23 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # leetcode-practice
+
 leetcodel练习题
---
+++ src/test/java/com/pengo/leetcodepractice/LeetcodePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pengo.leetcodepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
